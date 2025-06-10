9b4656411ab9f0c59d0a923091b9e5283243368f
SOURAV.RAJBANSHI <SOURAV.RAJBANSHI@sanimabank.com>
2025-06-10 16:40:57 +0545

    final

--- 100 days of python/Day1/band_name.py
+++
@@ -1,6 +0,0 @@
-# Band Name Generator
-print("Welcome to Band Name Generator!")
-city = input("Which city do you grew up in ?\n")
-pet_name = input("What is the name of your pet?\n")
-results = city + " " + pet_name
-print("Your band name could be: " + results)
--- 100 days of python/Day4/task.py
+++
@@ -1,6 +0,0 @@
-# import random
-# friends = ["Sourav", "Alice", "Bob","Charlie","David", "Emanuel"]
-# choosing = random.choice(friends)
-# print(choosing)
-
-
